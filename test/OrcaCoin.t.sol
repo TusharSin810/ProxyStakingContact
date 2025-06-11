3871edd2f707d176121fc808b7a2bc8c6632c13d
TusharSin810 <tsinghal666@gmail.com>
2025-06-11 14:31:20 +0530

    Completed Token Contract

--- test/OrcaCoin.t.sol
+++ test/OrcaCoin.t.sol
@@ -5,4 +5,37 @@ import "forge-std/Test.sol";
 
 import "src/OrcaCoin.sol";
 
-contract TestContract is Test {}
+contract OrcaCoinTest is Test {
+    OrcaCoinContract orcaCoin;
+
+    function setUp() public {
+        orcaCoin = new OrcaCoinContract(address(this));
+    }
+
+    function testInitialSupply() public view {
+        assert(orcaCoin.totalSupply() == 0);
+    }
+
+    function testFailMint() public {
+        vm.startPrank(0x9af180F5bBe5592b7c51D952f75f56125Bde9718);
+        orcaCoin.mint(0x9af180F5bBe5592b7c51D952f75f56125Bde9718, 10);
+    }
+
+    function testMint() public {
+        orcaCoin.mint(0x9af180F5bBe5592b7c51D952f75f56125Bde9718, 10);
+        assert(
+            orcaCoin.balanceOf(0x9af180F5bBe5592b7c51D952f75f56125Bde9718) == 10
+        );
+    }
+
+    function testChangeSatkingContract() public {
+        orcaCoin.updateStakingContract(
+            0x9af180F5bBe5592b7c51D952f75f56125Bde9718
+        );
+        vm.startPrank(0x9af180F5bBe5592b7c51D952f75f56125Bde9718);
+        orcaCoin.mint(0x9af180F5bBe5592b7c51D952f75f56125Bde9718, 10);
+        assert(
+            orcaCoin.balanceOf(0x9af180F5bBe5592b7c51D952f75f56125Bde9718) == 10
+        );
+    }
+}
